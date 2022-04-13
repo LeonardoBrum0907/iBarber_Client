77c0e5cb68e369de162ccc563c909dbce7792a6d
LeonardoBrum0907 <leobrum0907@gmail.com>
2022-04-13 19:23:10 -0300

    created logo and loader components

---
+++ src/components/Loader/index.tsx
@@ -0,0 +1,9 @@
+import { Container } from './loaderElements';
+
+export function Loader() {
+  return(
+    <Container>
+      <div className="loader" />
+    </Container>
+  );
+}
--- src/components/Logo/logoElements.tsx
+++ src/components/Logo/logoElements.tsx
@@ -4,5 +4,5 @@ export const Container = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-  margin-top: 30px;
+  margin-top: 29px;
 `;
